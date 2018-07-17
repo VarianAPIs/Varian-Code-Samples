2c9f42e1bb93c36b8a1925802b99607cc3941fec
Varian DW 2018 <jonathan.gelati@varian.com>
2018-07-17 14:33:28 +0000

    JWT token handler

---
+++ webinars & workshops/Developer Workshop 2018/Accessing ARIA with VAIS/Extensions/ResponseExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VMS.SF.Infrastructure.Contracts.Security;
+
+namespace Extensions
+{
+    public static class ResponseExtensions
+    {
+        public static string ToFormattedString(this GetPrivilegesResponse response)
+        {
+            var result = new StringBuilder();
+            result.AppendFormat("Privileges:{0}", Environment.NewLine);
+            foreach (var privilege in response.Privileges)
+            {
+                result.AppendFormat("\tPrivilege:{0}", Environment.NewLine);
+                result.AppendFormat("\t\tApplications:");
+                result.AppendFormat("{0}{1}"
+                    , string.Join(", ", privilege.Applications)
+                    , Environment.NewLine);
+
+                result.AppendFormat("\t\tGroupIds:");
+                result.AppendFormat("{0}{1}"
+                    , string.Join(", ", privilege.GroupIds)
+                    , Environment.NewLine);
+
+                result.AppendFormat("\t\tPrivileges: category:{0}, group:{1}, id:{2}, name:{3}, version:{4}{5}"
+                    , privilege.Privilege.Category
+                    , privilege.Privilege.Group
+                    , privilege.Privilege.Id
+                    , privilege.Privilege.Name
+                    , privilege.Privilege.Version
+                    , Environment.NewLine);
+
+
+            }
+            return result.ToString();
+        }
+
+    }
+}
---
+++ webinars & workshops/Developer Workshop 2018/Accessing ARIA with VAIS/Helpers/JWTTokenHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class JWTTokenHelper
+    {
+        public static string ReadToken(string token)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+        
+            return JsonHelper.FormatJson(jwtTokenHandler.ReadToken(token).ToString());
+        }
+    }
+}
