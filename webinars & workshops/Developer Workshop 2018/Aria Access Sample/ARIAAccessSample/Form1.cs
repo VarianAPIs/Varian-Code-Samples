a9c17d6f4d4be51540ed62a42138fbd291e90b19
Varian DW 2018 <jonathan.gelati@varian.com>
2018-07-19 13:32:07 +0000

    Aria Access Sample

---
+++ webinars & workshops/Developer Workshop 2018/Aria Access Sample/ARIAAccessSample/Form1.cs
@@ -0,0 +1,208 @@
+﻿using IdentityModel.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VMS.AWC.Link.MU3.WebService.Contracts;
+using VMS.AWC.Link.WebService.Contracts;
+
+namespace DevWorkshop2018AriaAcess
+{
+    public partial class Form1 : Form
+    {
+        private string _redirectUri;
+        private string _authority;
+        private string _clientIdentifier;
+        private string _scope;
+        private string _gatewayTokenUri;
+
+        static string _accessToken;
+        static string _identityToken;
+        static string _refreshToken;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _redirectUri = ConfigurationManager.AppSettings["RedirectUri"];
+            _authority = ConfigurationManager.AppSettings["Authority"];
+            _clientIdentifier = ConfigurationManager.AppSettings["ClientIdentifier"];
+            _scope = ConfigurationManager.AppSettings["Scope"];
+            _gatewayTokenUri = ConfigurationManager.AppSettings["GatewayTokenUri"];
+
+            Load += Authenticate;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchPatient_Click(object sender, EventArgs e)
+        {
+            String strLastName = txtLastName.Text.ToString().TrimEnd();
+            String strFirstName = txtFirstName.Text.ToString().TrimEnd();
+            //String token = _accessToken;
+            //String url = "https://ARIAV15TBox:55051/Gateway/Service.svc/token/Process";
+
+            txtPatientResponse.Text = processSearchPatientRequest(strLastName, strFirstName, _accessToken, _gatewayTokenUri);
+
+        }
+
+        private string processSearchPatientRequest(string strLastName, string strFirstName, String token, String url)
+        {
+            var request = new PatientSelectionRequest()
+            {
+                BirthDateFrom = null,
+                BirthDateTo = null,
+                FirstName = null,
+                LastName = null,
+                ExactSearch = null,
+                Sex = null
+            };
+            if (String.IsNullOrEmpty(strFirstName) == false)
+                request.FirstName = new VMS.AWC.Common.Contracts.String { Value = strFirstName };
+            if (String.IsNullOrEmpty(strLastName) == false)
+                request.LastName = new VMS.AWC.Common.Contracts.String { Value = strLastName };
+
+            var response = ARIAAccessHelper.SendRequestData(request, url, token);
+
+            return response;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void lblMachineId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCreatePatientFilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchAppointment_Click(object sender, EventArgs e)
+        {
+            String machineId = txtMachineId.Text.ToString().TrimEnd();
+            String deaprtmentName = "Oncology11";
+            String hospitalName = "AA_Hospital_1";
+            
+            txtApptResponse.Text = processSearchAppointmentRequest( machineId, hospitalName,  deaprtmentName, _accessToken, _gatewayTokenUri);
+        }
+
+        private string processSearchAppointmentRequest(string machineId, string hospitalName, string departmentname, string token, string url)
+        {
+            var request = new GetMachineAppointmentsRequest()
+            {
+                MachineId = new VMS.AWC.Common.Contracts.String() { Value = machineId},
+                HospitalName = new VMS.AWC.Common.Contracts.String() { Value = hospitalName },
+                DepartmentName = new VMS.AWC.Common.Contracts.String() { Value = departmentname}
+            };
+            
+
+            var response = ARIAAccessHelper.SendRequestData(request, url, token);
+
+            return response;
+        }
+
+        private void btnCreateAppointment_Click(object sender, EventArgs e)
+        {
+            string filePath = txtCreateApptFilePath.Text.ToString().TrimEnd();
+            string request = File.ReadAllText(filePath);
+            
+            
+            var response = ARIAAccessHelper.SendRequestData(request, _gatewayTokenUri, _accessToken);
+
+            txtCreateApptResp.Text = response;
+
+
+        }
+
+        private void btnCreatePatient_Click(object sender, EventArgs e)
+        {
+            string filePath = txtCreatePatientFilePath.Text.ToString().TrimEnd();
+            string request = File.ReadAllText(filePath);
+            
+
+            var response = ARIAAccessHelper.SendRequestData(request, _gatewayTokenUri, _accessToken);
+
+            txtCreatePatientResp.Text = response;
+        }
+
+        private void txtCreateApptFilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Authenticate(object sender, EventArgs e)
+        {
+            var browser = new SystemBrowser(_redirectUri);
+            var options = new OidcClientOptions
+            {
+                Authority = _authority,
+                ClientId = _clientIdentifier,
+                RedirectUri = _redirectUri,
+                Scope = "openid profile offline_access " + _scope,
+                FilterClaims = false,
+                Browser = browser
+            };
+            var oidcClient = new OidcClient(options);
+            var loginRequest = new LoginRequest();
+            var result = await oidcClient.LoginAsync(loginRequest);
+            if (result.IsError)
+            {
+                _accessToken = null;
+                _identityToken = null;
+                _refreshToken = null;
+
+                txtIdentityToken.Text = "Error";
+                txtAccessToken.Text = "Error";
+            }
+            else
+            {
+                _accessToken = result.AccessToken;
+                _identityToken = result.IdentityToken;
+                _refreshToken = result.RefreshToken;
+            }
+
+            txtIdentityToken.Text = JWTTokenHelper.ReadToken(_identityToken);
+            txtAccessToken.Text = JWTTokenHelper.ReadToken(_accessToken);
+        }
+
+        private async void RefreshTokens(object sender, EventArgs e)
+        {
+            var options = new OidcClientOptions
+            {
+                Authority = _authority,
+                ClientId = _clientIdentifier,
+                RedirectUri = _redirectUri
+            };
+            var oidcClient = new OidcClient(options);
+            var result = await oidcClient.RefreshTokenAsync(_refreshToken);
+            if (result.IsError)
+            {
+                txtIdentityToken.Text = "Error";
+                txtAccessToken.Text = "Error";
+            }
+            else
+            {
+                _accessToken = result.AccessToken;
+                _refreshToken = result.RefreshToken;
+                txtIdentityToken.Text = JWTTokenHelper.ReadToken(_identityToken);
+                txtAccessToken.Text = JWTTokenHelper.ReadToken(_accessToken);
+            }
+        }
+
+    }
+}
---
+++ webinars & workshops/Developer Workshop 2018/Aria Access Sample/ARIAAccessSample/Helper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.SF.Gateway.Contracts;
+
+namespace DevWorkshop2018AriaAcess
+{
+    class ARIAAccessHelper
+    {
+
+        private static string GetRequestJsonString(object request)
+        {
+            var dataContractJsonSerializer = new DataContractJsonSerializer(typeof(object),
+                new List<Type> { request.GetType(), typeof(MessageAttribute) });
+            var memoryStream = new MemoryStream();
+            dataContractJsonSerializer.WriteObject(memoryStream, request);
+            memoryStream.Flush();
+
+            var sJsonString = Encoding.UTF8.GetString(memoryStream.GetBuffer(), 0, (int)memoryStream.Position);
+            return sJsonString;
+        }
+
+        public static string SendRequestData(object objRequest, String gatewayUrl, String accessToken)
+        {
+            var request = GetRequestJsonString(objRequest);            
+            return SendRequestData(request, gatewayUrl, accessToken);
+
+        }
+
+        public static string SendRequestData(string request, String gatewayUrl, String accessToken)
+        {
+           var sResponse = String.Empty;
+            using (var c = new HttpClient())
+            {
+                //c.DefaultRequestHeaders.Add("ApiKey", ConfigurationManager.AppSettings["QppApiKeyWithLicense"]);
+                c.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                var task = c.PostAsync(gatewayUrl, new StringContent(request, Encoding.UTF8, "application/json"));
+                Task.WaitAll(task);
+                var responseTask = task.Result.Content.ReadAsStringAsync();
+                Task.WaitAll(responseTask);
+                sResponse = responseTask.Result;
+            }
+            return sResponse;
+
+        }
+    }
+}
